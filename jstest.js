dd51ee5bbd15595569e6e1ab5d490803cd4b9df1
MrStewartGithub <80249432+MrStewartGithub@users.noreply.github.com>
2021-03-10 22:48:11 +1100

    Add files via upload

---
+++ jstest.js
@@ -0,0 +1,73 @@
+/*Open when someone clicks on the span element */
+	function openNav1() {
+	  document.getElementById("myNav1").style.width = "100%";
+	}
+
+	function openNav2() {
+	  document.getElementById("myNav2").style.width = "100%";
+	}
+
+	function openNav3() {
+	  document.getElementById("myNav3").style.width = "100%";
+	}
+
+	function openNav4() {
+	  document.getElementById("myNav4").style.width = "100%";
+	}
+
+	function openNav5() {
+	  document.getElementById("myNav5").style.width = "100%";
+	}
+
+	function openNav6() {
+	  document.getElementById("myNav6").style.width = "100%";
+	}
+
+	function openNav7() {
+	  document.getElementById("myNav7").style.width = "100%";
+	}
+
+	function openNav8() {
+	  document.getElementById("myNav8").style.width = "100%";
+	}
+
+	function openNav9() {
+	  document.getElementById("myNav9").style.width = "100%";
+	}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav1() {
+  document.getElementById("myNav1").style.width = "0%";
+}// JavaScript Document
+
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+}
+		
+function closeNav3() {
+  document.getElementById("myNav3").style.width = "0%";
+}
+		
+function closeNav4() {
+  document.getElementById("myNav4").style.width = "0%";
+}
+		
+function closeNav5() {
+  document.getElementById("myNav5").style.width = "0%";
+}
+		
+function closeNav6() {
+  document.getElementById("myNav6").style.width = "0%";
+}
+		
+function closeNav7() {
+  document.getElementById("myNav7").style.width = "0%";
+}
+		
+function closeNav8() {
+  document.getElementById("myNav8").style.width = "0%";
+}
+		
+function closeNav9() {
+  document.getElementById("myNav9").style.width = "0%";
+}
